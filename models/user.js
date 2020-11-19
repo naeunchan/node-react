4c67a95f1774b55b06b5da76392774cb7354c37d
naeunchan <eunchan0492@naver.com>
2020-11-19 23:00:52 +0900

    logout

--- models/user.js
+++ models/user.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
 const saltRounds = 10
+const jwt = require("jsonwebtoken")
 
 const userSchema = mongoose.Schema({
     name: {
-        type: String,
-        maxlength: 50
-    },
-    password: {
         type: String,
         maxlength: 50
     },
@@ -15,6 +12,10 @@ const userSchema = mongoose.Schema({
         type: String,
         trim: true,
         unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
     },
     lastname: {
         type: String,
@@ -48,10 +49,53 @@ userSchema.pre("save", function (next) {
                 next()
             })
         })
+    } else {
+        next()
     }
 })
 
-const User = mongoose.model("User", userSchema);
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    // plainPassword와 암호화된 비밀번호(DB) 비교
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err)
+            return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function (cb) {
+    let user = this
+
+    // jsonwebtoken을 이용하여 token 생성
+    let token = jwt.sign(user._id.toHexString(), "secretToken")
+
+    user.token = token
+    user.save(function (err, user) {
+        if (err)
+            return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function (token, cb) {
+    let user = this
+
+    // 토큰 decode
+    jwt.verify(token, "secretToken", function (err, decoded) {
+        // 유저 ID 이용하여 유저 찾기
+        // 그 후 클라이언트에서 가져온 토큰과 DB에 보관된 토큰 일치하는지 확인
+        user.findOne({
+            "_id": decoded,
+            "token": token
+        }, function (err, user) {
+            if (err)
+                return cb(err)
+            cb(null, user)
+        })
+    })
+}
+
+const User = mongoose.model("User", userSchema)
 module.exports = {
     User
 }
