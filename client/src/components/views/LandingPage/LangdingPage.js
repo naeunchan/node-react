18ef291e653ac1a019f46a877b6b04b9b020faf6
naeunchan <eunchan0492@naver.com>
2020-11-29 23:05:46 +0900

    login

--- client/src/components/views/LandingPage/LangdingPage.js
+++ client/src/components/views/LandingPage/LangdingPage.js
@@ -6,7 +6,19 @@ function LangdingPage() {
 		axios.get("/api/hello").then(response => console.log(response.data));
 	}, []);
 
-	return <div>LandingPage입니다</div>;
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				width: "100%",
+				height: "100vh",
+			}}
+		>
+			<h2>시작 페이지</h2>
+		</div>
+	);
 }
 
 export default LangdingPage;
